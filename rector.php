a8fafab6e48caeb299af5eb381e15ae9bc3da2d5
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-25 12:49:17 +0300

    fix

--- rector.php
+++ rector.php
@@ -6,7 +6,6 @@ use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
-use Rector\Php82\Rector\Param\AddSensitiveParameterAttributeRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -14,12 +13,6 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     ->withPhpSets(php82: true)
-    ->withConfiguredRule(
-        AddSensitiveParameterAttributeRector::class,
-        [
-            'sensitive_parameters' => ['token', 'providerToken', 'secretToken'],
-        ]
-    )
     ->withSkip([
         ClosureToArrowFunctionRector::class,
         NullToStrictStringFuncCallArgRector::class,
